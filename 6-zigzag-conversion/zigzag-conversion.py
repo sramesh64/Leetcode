6fae6cee8d9f542fe8d0de74c16a192de53b2b85
Sanjay Ramesh <167709587+sramesh64@users.noreply.github.com>
2025-10-26 17:55:34 -0400

    Time: 6 ms (90.32%) | Memory: 17.8 MB (69.31%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,20 +1,19 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1 or numRows >= len(s):
+        if(numRows == 1):
             return s
-
+        row = 0
+        dir = 1
         rows = [''] * numRows
-        row, direction = 0, 1
+        for char in s:
+            rows[row] += char
 
-        for ch in s:
-            rows[row] += ch
+            if(row == numRows - 1):
+                dir = -1
+            elif(row == 0):
+                dir = 1
 
-            # reverse direction at the top or bottom
-            if row == 0:
-                direction = 1
-            elif row == numRows - 1:
-                direction = -1
-
-            row += direction
-
+            row += dir
+        
         return ''.join(rows)
+
