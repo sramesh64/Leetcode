8cd84b2c18c584ee4b5b0913317d8ec0b94ddf72
Sanjay Ramesh <167709587+sramesh64@users.noreply.github.com>
2025-11-08 18:17:24 -0500

    Time: 0 ms (100.00%) | Memory: 12.7 MB (76.40%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        
+        res = []
+
+        curr_string = ""
+
+        def dfs(curr_string, num_opened, num_closed):
+            if(num_closed > num_opened or num_opened > n or num_closed > n):
+                return
+            if(num_opened == n and num_closed == n):
+                res.append(curr_string)
+                return
+
+            curr_string += "("
+            dfs(curr_string, num_opened + 1, num_closed)
+
+            curr_string = curr_string[:-1]
+
+            curr_string += ")"
+            dfs(curr_string, num_opened, num_closed + 1)
+
+            curr_string = curr_string[:-1]
+
+            return
+
+        dfs(curr_string, 0, 0)
+
+        return res
